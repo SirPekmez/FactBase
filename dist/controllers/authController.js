29b92c101e61c74ce14d64a7283986b8a55f4bce
Zu Hause <tradluck@MacBook-Pro-von-Zu.local>
2026-08-10 18:28:14 +0200

    hore: stop tracking build artifacts (RCV-004)

--- dist/controllers/authController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logout = logout;
-function logout(req, res) {
-    return res.status(200).json({
-        message: "Logout erfolgreich",
-    });
-}
